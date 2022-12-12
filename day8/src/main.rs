5903a02952627771c23965855592fb2ee4598e6a
crippa1337 <cristopher.torgrip@elev.ga.ntig.se>
2022-12-12 22:03:34 +0100

    day8 complete

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input").unwrap();
+    let mut answer: u32 = 0;
+    for line in input.lines() {
+        let vec: Vec<&str> = line.split(", ").collect();
+        let num_vec: Vec<u32> = vec
+            .into_iter()
+            .map(|string| string.parse::<u32>().unwrap())
+            .collect();
+
+        if !num_vec.contains(&7) {
+            let volume: u32 = num_vec
+                .iter()
+                // Could use .product() here
+                .fold(1, |product, measurement| measurement * product);
+            answer += volume;
+        }
+    }
+
+    println!("{answer}");
+    // 59193
 }
