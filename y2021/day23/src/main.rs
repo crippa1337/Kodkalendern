f75b5802bf6a37f1909810b8410057cd0ecbcbbe
Cristopher T <cristopher.torgrip@elev.ga.ntig.se>
2022-12-13 16:19:11 +0100

    day23 complete

--- y2021/day23/src/main.rs
+++ y2021/day23/src/main.rs
@@ -1,15 +1,29 @@
-use std::{fs, vec};
+use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input").unwrap();
 
     for line in input.lines() {
         let mut line_chars: Vec<char> = vec![];
+        let mut sorted_chars: Vec<char> = vec![];
 
         for chars in line.chars() {
-            line_chars.push(chars)
+            line_chars.push(chars);
+            sorted_chars.push(chars);
+        }
+        sorted_chars.sort();
+
+        let length = line_chars.len();
+        if line_chars[length - 1].is_alphabetic() && line_chars[length - 2].is_alphabetic() {
+            println!("Answer: {line}");
+            // Doesn't return anything
+        }
+
+        for (char1, char2) in line_chars.iter().zip(sorted_chars.iter()) {
+            if char1 != char2 {
+                println!("Answer: {line}")
+                // 124379ö
+            }
         }
     }
 }
-
-// Input is incorrectly parsed
