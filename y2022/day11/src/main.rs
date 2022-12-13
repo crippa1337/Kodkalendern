16cf82423040f5c2ca170a403b6f233a31dd1bdf
Cristopher T <cristopher.torgrip@elev.ga.ntig.se>
2022-12-13 09:52:47 +0100

    day11 complete

---
+++ y2022/day11/src/main.rs
@@ -0,0 +1,18 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input").unwrap();
+    let mut answer: u32 = 0;
+
+    for line in input.lines() {
+        let vec: Vec<&str> = line.split(", ").collect();
+        for (char1, char2) in vec[0].chars().zip(vec[1].chars()) {
+            if char1 != char2 {
+                answer += 1;
+            }
+        }
+    }
+
+    println!("{answer}");
+    // 856 (Input is incorrect)
+}
